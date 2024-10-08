86b995efff4d53ade0ce403ef44c998f9c7370da
幸大 畠山 <230169@jc-21.jp>
2024-10-08 16:22:59 +0900

    たまに立ち止まったり、進んだり

--- Assets/Enemy/Scripts/PatrolAI.cs
+++ Assets/Enemy/Scripts/PatrolAI.cs
@@ -6,6 +6,7 @@ public class PatrolAI : MonoBehaviour
 {
     private bool movingRight = true;
     private bool isAnimation2Playing = false;
+    private bool isAnimation3Playing = false; // ANIMATION_3のフラグ
     SpriteRenderer sr;
 
     [Tooltip("動くスピード")] public float speed;
@@ -15,45 +16,77 @@ public class PatrolAI : MonoBehaviour
     [Tooltip("吹き飛ばす力")] public float thrust = 10f;
     [Tooltip("右方向に吹き飛ばす力")] public Vector2 forceDirectionRight = new Vector2(0.3f, 1f);
     [Tooltip("左方向に吹き飛ばす力")] public Vector2 forceDirectionLeft = new Vector2(-0.3f, 1f);
+    [Tooltip("アニメーション切り替え間隔の最小値")] public float minAnimationSwitchInterval = 1f;
+    [Tooltip("アニメーション切り替え間隔の最大値")] public float maxAnimationSwitchInterval = 3f;
 
     [Space(20)]
 
-    [Header("アニメーション用配列_1 ----------------------------------------------------")]
+    [Header("アニメーション配列_1_Walk ------------------------------------------------")]
     [Tooltip("配列数")] public Sprite[] anim_1_array;
     float anime_time_1;
     [Tooltip("フレームが変わる間隔設定")] public float anim_1_sec;
     [Tooltip("スプライトの変化プレビュー")] public int anime_1_count;
 
-    [Header("アニメーション用配列_2 ----------------------------------------------------")]
+    [Header("アニメーション用配列_2_Dead ------------------------------------------------")]
     [Tooltip("配列数")] public Sprite[] anim_2_array;
     float anime_time_2;
     [Tooltip("フレームが変わる間隔設定")] public float anim_2_sec;
     [Tooltip("スプライトの変化プレビュー")] public int anime_2_count;
 
+    [Header("アニメーション用配列_3_Stay ------------------------------------------------")]
+    [Tooltip("配列数")] public Sprite[] anim_3_array;
+    float anime_time_3;
+    [Tooltip("フレームが変わる間隔設定")] public float anim_3_sec;
+    [Tooltip("スプライトの変化プレビュー")] public int anime_3_count;
+
+    private float animationSwitchTime;
+
     void Start()
     {
         sr = this.GetComponent<SpriteRenderer>();
+        SetRandomAnimationSwitchTime();
     }
 
     private void Update()
     {
-        if (!isAnimation2Playing)
+        if (isAnimation2Playing)
         {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            ANIMATION_2();
+            return;
+        }
 
-            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
-            Debug.DrawRay(groundDetection.position, Vector2.down * distance, Color.red);
+        if (Time.time >= animationSwitchTime)
+        {
+            isAnimation3Playing = !isAnimation3Playing;
+            SetRandomAnimationSwitchTime();
+        }
 
-            if (groundInfo.collider == null)
-            {
-                FlipDirection();
-            }
-
-            ANIMATION_1();
+        if (isAnimation3Playing)
+        {
+            ANIMATION_3();
         }
         else
         {
-            ANIMATION_2();
+            ANIMATION_1();
+            Move();
+        }
+    }
+
+    private void SetRandomAnimationSwitchTime()
+    {
+        animationSwitchTime = Time.time + Random.Range(minAnimationSwitchInterval, maxAnimationSwitchInterval);
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        Debug.DrawRay(groundDetection.position, Vector2.down * distance, Color.red);
+
+        if (groundInfo.collider == null)
+        {
+            FlipDirection();
         }
     }
 
@@ -122,6 +155,19 @@ public class PatrolAI : MonoBehaviour
             }
 
             sr.sprite = anim_2_array[anime_2_count];
+        }
+    }
+
+    void ANIMATION_3()
+    {
+        if (Time.time - anime_time_3 > anim_3_sec)
+        {
+            anime_time_3 = Time.time;
+
+            anime_3_count++;
+            if (anime_3_count >= anim_3_array.Length) { anime_3_count = 0; }
+
+            sr.sprite = anim_3_array[anime_3_count];
         }
     }
 }
